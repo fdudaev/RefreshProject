9a184d3b6cc369658a09a8ca95597dc3b7a019bc
fdudaev <61507529+fdudaev@users.noreply.github.com>
2022-12-30 06:12:41 -0500

    commiting changes to LotteryTicket class

--- src/LotteryTicket.java
+++ src/LotteryTicket.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Arrays;
 
 public class LotteryTicket {
 
@@ -8,6 +9,7 @@ public class LotteryTicket {
     public static void main(String[] args) {
 
         int [] ticket = generateNumber();
+        Arrays.sort(ticket);
         printTicket(ticket);
 
     }
@@ -17,9 +19,32 @@ public class LotteryTicket {
         Random random = new Random();
 
         for (int i = 0; i < LENGTH; i++){
-            ticket[i] = random.nextInt(MAX_TICKET_NUMBER)+1;
+            int randomNumber;
+            //Generate random number, then search to make sure it doesn't already exist in the array.
+            //If it does, regenerate and search again.
+            do{
+                randomNumber = random.nextInt(MAX_TICKET_NUMBER)+1;
+            }while (search(ticket, randomNumber));
+            ticket[i] =randomNumber;
         }
         return ticket;
+    }
+
+    /**
+     * Does a sequential search on the array to find a value
+     * @param array Array to search through
+     * @param numberToSearchFor  Value to search for
+     * @return true if found, false if not
+     */
+    public static boolean search(int[] array, int numberToSearchFor){
+        for (int value : array){
+            if (value == numberToSearchFor) {
+                return true;
+            }
+        }
+        // If we've reached this point, then the entire array was searched
+        // and the value wasn't found.
+        return false;
     }
     public static void printTicket(int [] tickets){
 
