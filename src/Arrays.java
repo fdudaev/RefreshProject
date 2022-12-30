5db229c7767c0365dd0644e84d45ea9e686e1fe5
fdudaev <61507529+fdudaev@users.noreply.github.com>
2022-12-30 06:14:06 -0500

    System.nanoTime()

--- src/Arrays.java
+++ src/Arrays.java
@@ -5,12 +5,17 @@ public class Arrays {
         int [] lottoTicket = new int[6];
         String  [] names = {"Bilal", "Aisha", "Aliya"};
 
+        double s = System.nanoTime();
         lottoTicket[0] = 2;
         lottoTicket[2] = 99;
         lottoTicket[3] = 99;
         lottoTicket[4] = 99;
         lottoTicket[5] = 99;
         lottoTicket[2] = 33;
+        double e = System.nanoTime();
+        double total = s+e;
+
+        System.out.println("total time executed " + total);
 
         for (int i = 0; i < lottoTicket.length; i++){
             System.out.println(lottoTicket[i]);
